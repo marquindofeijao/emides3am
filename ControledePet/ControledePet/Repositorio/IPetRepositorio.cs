1a04b41be84eae5972a9c60c13781cc2fe1b59af
Marcos Rodrigues <marcos.mendonca@aluno.senai.br>
2023-08-25 16:11:05 -0300

    Crud

---
+++ ControledePet/ControledePet/Repositorio/IPetRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ControledePet.Models;
+
+namespace ControledePet.Repositorio
+{
+    public interface IPetRepositorio
+    {
+        PetModel ListarPorId(int id);
+        List<PetModel> BuscarTodos();
+        PetModel Adicionar(PetModel pet);
+        PetModel Atualizar(PetModel pet);
+        bool Apagar(int id);
+    }
+}
